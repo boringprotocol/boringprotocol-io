a8803be5a2b5d5058e95b221fc7ed19901a673ca
whaleen <joshuavaage@gmail.com>
2024-04-12 21:16:39 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -118,7 +118,7 @@ const Home: NextPage = () => {
 
     <>
       <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="p-8 bg-black rounded-lg shadow-lg max-w-md mx-auto">
+        <div className="flex items-center justify-center relative w-full h-full before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-gray-200 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#2e2e2e] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
           <article className="font-jetbrains">
 
 
